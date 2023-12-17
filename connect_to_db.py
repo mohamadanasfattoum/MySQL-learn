2e610cb6c41962dba2510c9852f2f989b4878e9f
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-17 15:15:07 +0100

    import mysql connector

--- connect_to_db.py
+++ connect_to_db.py
@@ -1,1 +1,18 @@
-from .env import DATABASE_PASSWORD
+from .secrets import DATABASE_PASSWORD
+import mysql.connector
+
+myconn = mysql.connector.connect(
+    host="localhost" ,
+    user = "root" ,
+    passwd = DATABASE_PASSWORD, 
+    database = 'dbsql'
+)
+
+
+mycursor = myconn.cursor()
+
+
+cursorobject.execute('CREATE DATABASE')
+
+
+print('All Done!')
