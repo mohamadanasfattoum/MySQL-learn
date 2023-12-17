d05b8e02a86502c471c9ac84e81d0c62e3b9f7d5
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-17 21:06:21 +0100

    add und run 5_insert_data.py die daten sind in db nicht gespeichert

---
+++ 5_insert_data.py
@@ -0,0 +1,19 @@
+from project.secrets import DATABASE_PASSWORD
+import mysql.connector
+
+myconn = mysql.connector.connect(
+    host = "localhost" ,
+    user = "root" ,
+    passwd = DATABASE_PASSWORD, 
+    database = 'mydb'
+)
+
+mycursor = myconn.cursor()
+
+sql = 'INSERT INTO products(name , description) VALUES(%s ,%s)'
+data = ('xiaomi 12t' , 'Spitzenkameras, erstklassige Displays und hohe Geschwindigkeit: Xiaomi Handys punkten in jeder Hinsicht.')
+
+mycursor.execute(sql , data)
+
+
+print ('insert was successfully')
