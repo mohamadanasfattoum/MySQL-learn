d9c54f4a2d37bffbb1ca86d9291b30e3b4d84723
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-18 17:21:44 +0100

    add & run 15_select_with_limit.py

---
+++ 15_select_with_limit.py
@@ -0,0 +1,24 @@
+from project.secrets import DATABASE_PASSWORD
+import mysql.connector
+
+myconn = mysql.connector.connect(
+    host = "localhost" ,
+    user = "root" ,
+    passwd = DATABASE_PASSWORD, 
+    database = 'mydb'
+)
+
+mycursor = myconn.cursor()
+
+mycursor.execute(' SELECT * FROM products  LIMIT 5') # descent ترتيب تنازلي 
+
+myresult = mycursor.fetchall()
+
+
+for product in myresult:
+    print(product)
+
+# print (myresult)
+
+
+print ('selected successfully')
