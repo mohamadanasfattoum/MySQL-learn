f0f1199c78ca01a5ee916a5aefd198a336d301c9
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-19 00:52:38 +0100

    add & run 21_drop_table.py

---
+++ 21_drop_table.py
@@ -0,0 +1,20 @@
+from project.secrets import DATABASE_PASSWORD
+import mysql.connector
+
+myconn = mysql.connector.connect(
+    host = "localhost" ,
+    user = "root" ,
+    passwd = DATABASE_PASSWORD, 
+    database = 'mydb'
+)
+
+mycursor = myconn.cursor()
+
+
+mycursor.execute('DROP TABLE brand') 
+# mycursor.execute('DROP TABLE IF EXIST') 
+
+
+myconn.commit()
+
+print ('deleted successfully')
