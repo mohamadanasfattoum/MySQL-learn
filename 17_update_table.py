27a293ec2bde3cdfebcad7fc764adb1482296b99
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-18 23:49:39 +0100

    add & run 17_update_table.py to update price

---
+++ 17_update_table.py
@@ -0,0 +1,19 @@
+from project.secrets import DATABASE_PASSWORD
+import mysql.connector
+
+myconn = mysql.connector.connect(
+    host = "localhost" ,
+    user = "root" ,
+    passwd = DATABASE_PASSWORD, 
+    database = 'mydb'
+)
+
+mycursor = myconn.cursor()
+
+
+mycursor.execute('UPDATE products SET price = "400" WHERE name = "xiaomi 12t" ')
+
+myconn.commit()
+
+print ('updated successfully')
+
