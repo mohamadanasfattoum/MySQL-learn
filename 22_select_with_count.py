609eac7a71450587f23d09026477da5dc305c76d
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-19 14:59:34 +0100

    add & run 22_select_with_count.py

---
+++ 22_select_with_count.py
@@ -0,0 +1,18 @@
+from project.secrets import DATABASE_PASSWORD
+import mysql.connector
+
+myconn = mysql.connector.connect(
+    host = "localhost" ,
+    user = "root" ,
+    passwd = DATABASE_PASSWORD, 
+    database = 'mydb'
+)
+
+mycursor = myconn.cursor()
+
+
+mycursor.execute('SELECT COUNT(*) FROM products') 
+myresult = mycursor.fetchone()
+print (myresult)
+
+print ('counted successfully')
